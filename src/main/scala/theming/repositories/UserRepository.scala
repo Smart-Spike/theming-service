b359e1c14d3040c4c741e66326acb126bf77edcb
schekalin <stanislav.chekalin@sigfig.com>
2018-07-12 15:38:56 +0100

    refactored UserRepository to use custom column mapper for roles

--- src/main/scala/theming/repositories/UserRepository.scala
+++ src/main/scala/theming/repositories/UserRepository.scala
@@ -1,5 +1,6 @@
 package theming.repositories
 
+import slick.ast.BaseTypedType
 import slick.jdbc.MySQLProfile.api._
 import slick.lifted.Tag
 import theming.domain.Roles.Role
@@ -15,7 +16,7 @@ class UserRepository(db: Database)(implicit val executionContext: ExecutionConte
     val userId = generateId
     val userWithId = user.copy(id = Some(userId))
     val createUser = users += createRecord(userWithId)
-    val createRoles = userRoles ++= user.roles.map(role => UserRole(userId, role.name))
+    val createRoles = userRoles ++= user.roles.map(role => UserRole(userId, role))
     db.run((createUser andThen createRoles).transactionally).map(_ => userWithId)
   }
 
@@ -30,18 +31,18 @@ class UserRepository(db: Database)(implicit val executionContext: ExecutionConte
   private def findByPredicate(predicate: Users => Rep[Option[Boolean]]): Future[Option[User]] = {
     val usersJoinCompaniesJoinRoles =
       (users.filter(predicate)
-        joinLeft companies on(_.companyId === _.id)
+        joinLeft companies on (_.companyId === _.id)
         joinLeft userRoles on (_._1.id === _.userId))
-      .map {
-        case ((userRecord, companyRecord), userRoleRecord) => (userRecord, companyRecord, userRoleRecord.map(_.role))
-      }
+        .map {
+          case ((userRecord, companyRecord), userRoleRecord) => (userRecord, companyRecord, userRoleRecord.map(_.role))
+        }
 
     db.run(usersJoinCompaniesJoinRoles.result).map {
       case Nil => None
       case userRecordsCompaniesAndRoles =>
         val (userRecord, company, _) = userRecordsCompaniesAndRoles.head
         val roles = userRecordsCompaniesAndRoles collect {
-          case (_, _, Some(roleName)) => Role(roleName)
+          case (_, _, Some(role)) => role
         }
         Some(createUser(userRecord, company, roles))
     }
@@ -69,12 +70,15 @@ class UserRepository(db: Database)(implicit val executionContext: ExecutionConte
 
   private val users = TableQuery[Users]
 
-  private case class UserRole(userId: String, role: String)
+  private case class UserRole(userId: String, role: Role)
+
+  private implicit val roleMapper: BaseTypedType[Role] =
+    MappedColumnType.base[Role, String](_.name, Role.apply)
 
   private class UserRoles(tag: Tag) extends Table[UserRole](tag, "user_roles") {
     def userId = column[String]("user_id")
 
-    def role = column[String]("role")
+    def role = column[Role]("role")
 
     def * = (userId, role).mapTo[UserRole]
   }
